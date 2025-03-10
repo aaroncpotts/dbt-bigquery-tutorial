ac852382df7b18460e0005987045f5667cc56d36
Aaron Potts <aaron.potts@theinformationlab.com>
2025-03-10 14:24:47 +0000

    best practices CTE cleanup on total_revenue

--- analysis/total_revenue.sql
+++ analysis/total_revenue.sql
@@ -1,5 +1,7 @@
-with payments as (select * from {{ ref('stg_stripe__payments') }})
+with payments as (select * from {{ ref('stg_stripe__payments') }}),
 
-select sum(amount) as total_successful_payments
+aggregated as (select sum(amount) as total_successful_payments
 from payments
-where status = 'success'
+where status = 'success')
+
+select * from aggregated
