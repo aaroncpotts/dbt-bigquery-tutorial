154559e1938d0ec7c0f4cf7618a51389a289b46d
Aaron Potts <aaron.potts@theinformationlab.com>
2025-03-10 14:07:57 +0000

    Built data model for testing, added employees seed, integrated employees seed into testing data model

--- analysis/orders_by_day.sql
+++ analysis/orders_by_day.sql
@@ -1,26 +1,28 @@
-with orders as(
-select * from {{ ref('stg_jaffle_shop__orders') }}
-),
+with
+    orders as (select * from {{ ref("stg_jaffle_shop__orders") }}),
 
-daily as (
-    select 
-        order_date,
-        count(*) as num_orders,
-    
-    {% for order_status in ['returned', 'completed', 'return_pending', 'shipped', 'placed'] %}
-        sum(case when order_status = {{order_status}} then 1 else 0 end) as {{ order_status }}_total {{',' if not loop.last}}
-    {%endfor%}
+    daily as (
+        select
+            order_date,
+            count(*) as num_orders,
 
-    from orders
-    group by 1
-),
+            {% for order_status in [
+                "returned",
+                "completed",
+                "return_pending",
+                "shipped",
+                "placed",
+            ] %}
+                sum(
+                    case when order_status = '{{ order_status }}' then 1 else 0 end
+                ) as {{ order_status }}_total {{ ',' if not loop.last }}
+            {% endfor %}
 
-compared as (
-    select *,
 
-        lag(num_orders) over (order by order_date) as previous_day_orders
-    from daily
-)
 
-select * from compared
+        from orders
+        group by 1
+    )
 
+select *
+from daily
